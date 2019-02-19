2d099b9f82c522f46edf3e65c525ec95af6441ba
Aaron Hellman <ahellman21@gmail.com>
2019-02-18 18:34:34 -0600

    Add files via upload

--- src/init.js
+++ src/init.js
@@ -5,4 +5,63 @@ var personal_info = {
   ,jobTitle: "Data Analyst"
   ,homeOffice: "Domain D2"
 };
-// ,tellMeMore: "Hi! Welcome to the second website I have ever built. I am currently a data analyst in financial planning and analysis that is implementing machine learning and data science with Python in our fanatical forecasts. I like what I am doing and am happy that it is getting me on a course to be a Data Scientist but feel that I need more technical knowledge to further support my team. That is the reason that I am applying for this course. I hope you like the site! "
+
+// var bookJSON =
+// fetch('https://openlibrary.org/api/books?bibkeys=OLID:OL262758W&format=json&jscmd=data')
+// ;
+// function api_fetch() {
+//   fetch('https://openlibrary.org/api/books?bibkeys=OLID:OL262758W&format=json&jscmd=data')
+//     .then(function(response) {
+//       var bookJSON = response.json()
+//       return bookJSON;
+//     })
+//     // .then(function(myJson) {
+//     //   console.log(JSON.stringify(bookJSON));
+//     // });
+// }
+//
+// // const userAction = async () => {
+// //     const response = await fetch('http://example.com/movies.json');
+// //     const myJson = await response.json(); //extract JSON from the http response
+// //     // do something with myJson
+// //   }
+//
+// // var bookJAVAS = JSON.parse(bookJSON);
+//
+// function apiFunc() {
+//   document.getElementById("api_title_txt").innerHTML = bookJSON.title;
+//   document.getElementById("api_image").innerHTML = bookJSON.cover.small;
+//   document.getElementById("api_author_txt").innerHTML = bookJSON.authors.name;
+// };
+//
+// const ul = document.getElementById('bookAPI');
+// const url = 'https://openlibrary.org/api/books?bibkeys=OLID:OL262758W&format=json&jscmd=data';
+// fetch(url)
+// .then((resp) => resp.json())
+// .then(function(data) {
+//   let authors = data.results;
+//   return authors.map(function(author) {
+//     let li = createNode('li'),
+//         img = createNode('img'),
+//         span = createNode('span');
+//     img.src = author.picture.medium;
+//     span.innerHTML = `${author.name.first} ${author.name.last}`;
+//     append(li, img);
+//     append(li, span);
+//     append(ul, li);
+//   })
+// })
+//
+// // Import socket.io with a connection to a channel (i.e. tops)
+// const socket = require('socket.io-client')('https://ws-api.iextrading.com/1.0/tops')
+// // Listen to the channel's messages
+// socket.on('message', message => console.log(message))
+// // Connect to the channel
+// socket.on('connect', () => {
+//   // Subscribe to topics (i.e. appl,fb,aig+)
+//   socket.emit('subscribe', 'expe')
+//   // Unsubscribe from topics (i.e. aig+)
+//   socket.emit('unsubscribe', 'aig+')
+// })
+// // Disconnect from the channel
+// socket.on('disconnect', () => console.log('Disconnected.'))
